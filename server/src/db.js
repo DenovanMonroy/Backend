e73cf3516ae6964a0810758888a32b193bca55e3
Denovan Monroy Ruiz <129570460+DenovanMonroy@users.noreply.github.com>
2025-03-12 04:15:20 +0000

    Add files via upload

---
+++ server/src/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/merndb');
+        console.log('>>> DB is connected');
+
+    } catch (error) {
+        console.log('>>> Error connecting to DB');
+        console.log(error);
+    }
+}
+
+export { connectDB };
